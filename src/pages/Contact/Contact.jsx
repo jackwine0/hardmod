3bec35987476ca4ac0d96cf2e20bc9f0f3de822c
jackwine0 <samakande98@gmail.com>
2024-08-23 22:14:03 +0100

    Batman 1

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,61 +1,47 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
-import './Contact.css';
 import Guarantee from '../../components/Gurantee/gurantee';
-
+import "./Contact.css"
 const Contact = () => {
   return (
-    <div>
-      <div>
-        <Hero />
-      </div>
-      <div className="intro">
-        <h2>Get In Touch With Us</h2>
-        <p>For more information about our products & services, please feel free to drop us an email.<br /> Our staff will always be there to help you out. Do not hesitate!</p>
-      </div>
+    <section className="contact-section">
+      <Hero/>
       <div className="container">
         <div className="contact-info">
-          <ul>
-            <li>
-              <label>Address:</label>
-              <p>123 Main St, Anytown, USA</p>
-            </li>
-            <li>
-              <label>Phone:</label>
-              <p>555-555-5555</p>
-            </li>
-            <li>
-              <label>Working Time:</label>
-              <p>Mon-Fri, 9am-5pm</p>
-            </li>
-          </ul>
+          <p className="contact-text">
+            We're here to assist you! If you have any questions or need assistance, please feel free to reach out to
+            us.
+            <br /><br />
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, rerum?            <a href="https://veilmail.io/e/FkKh7o" className="email-link">https://user@example.com</a>
+          </p>
+          <p className="contact-text">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, dolorum!          </p>
         </div>
         <div className="contact-form">
-          <form>
-            <div className="input-container">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" className="input" />
+          <h1 className="contact-title">Contact Form</h1>
+          <form action="send-contact.php" method="post" id="submit-contact-form">
+            <div className="form-group">
+              <label htmlFor="name" className="label">Your Name</label>
+              <input type="text" id="name" name="name" required="" className="input-field" />
             </div>
-            <div className="input-container">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" className="input" />
+            <div className="form-group">
+              <label htmlFor="email" className="label">Your Email</label>
+              <input type="email" id="email" name="email" required="" className="input-field" />
             </div>
-            <div className="input-container">
-              <label htmlFor="subject">Subject:</label>
-              <input type="text" id="subject" name="subject" className="input" />
+            <div className="form-group">
+              <label htmlFor="message" className="label">Your Message</label>
+              <textarea id="message" name="message" required="" className="input-field" />
             </div>
-            <div className="input-container textarea">
-              <label htmlFor="message">Message:</label>
-              <textarea id="message" name="message" className="input"></textarea>
+            <div className="form-group">
+              <button type="submit" className="submit-button">
+                Send Message ✉
+              </button>
             </div>
-            <input type="submit" value="Send" className="btn" />
           </form>
         </div>
       </div>
-      <div>
-        <Guarantee/>
-      </div>
-    </div>
+      <Guarantee/>
+    </section>
   );
 };
 
